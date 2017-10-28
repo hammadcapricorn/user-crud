0142f9ed95972a94092c44c0eff15bf9d5582823
hfs <goshi.hammad@gmail.com>
2017-10-28 19:02:32 +0500

    add contact & listed in table through server

--- index.js
+++ index.js
@@ -23,5 +23,34 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 
+app.get('/contactlist', function (req, res) {
+    console.log('I received a GET request');
+
+    db.contactlist.find(function (err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+});
+
+app.post('/contactlist', function (req, res) {
+    console.log(req.body);
+    db.contactlist.insert(req.body, function(err, doc) {
+        res.json(doc);
+    });
+});
+
+
+app.get('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+        res.json(doc);
+    });
+});
+
+
 app.listen(3000);
-console.log("Server running on port 3000");
+console.log("Server running on port 3000")
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log('listening on', http.address().port);
+// });
--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,29 @@
 /**
  * Created by hfs on 10/28/2017.
  */
+var userCrud = angular.module('userCrud', []);
+
+userCrud.controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
+        console.log("Hello from app controller");
+
+    var refresh = function() {
+        $http.get('/contactlist').success(function(response) {
+            console.log("got the requested data");
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    };
+
+    refresh();
+
+    $scope.addContact = function() {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function(response) {
+            console.log(response);
+            refresh();
+
+        });
+    };
+    
+
+}]);﻿
